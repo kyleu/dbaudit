4242286e51b2a15e16c3a9372b482319556600aa
Kyle Unverferth <kyle@kyleu.com>
2024-03-17 09:23:25 -0600

    Timestamp script improvements

--- client/src/client.ts
+++ client/src/client.ts
@@ -16,7 +16,8 @@ import {appInit} from "./app";
 declare global {
   interface Window { // eslint-disable-line @typescript-eslint/consistent-type-definitions
     "dbaudit": {
-      relativeTime: (time: string, el?: HTMLElement) => string;
+      wireTime: (el: HTMLElement) => void;
+      relativeTime: (el: HTMLElement) => string;
       autocomplete: (el: HTMLInputElement, url: string, field: string, title: (x: unknown) => string, val: (x: unknown) => string) => void;
       setSiblingToNull: (el: HTMLElement) => void;
       initForm: (frm: HTMLFormElement) => void;
@@ -29,8 +30,10 @@ declare global {
 
 export function init(): void {
   const [s, i] = formInit();
+  const [wireTime, relativeTime] = timeInit();
   window.dbaudit = {
-    relativeTime: timeInit(),
+    wireTime: wireTime,
+    relativeTime: relativeTime,
     autocomplete: autocompleteInit(),
     setSiblingToNull: s,
     initForm: i,
